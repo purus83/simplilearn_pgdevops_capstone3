0ec89b127c828c9dbd9e85e6e7c2c83c9ebdf045
purus83 <101929676+purus83@users.noreply.github.com>
2022-09-21 13:45:09 +0530

    Update update_docker_image_deployment_yaml_file.sh

--- gs-spring-boot-docker/update_docker_image_deployment_yaml_file.sh
+++ gs-spring-boot-docker/update_docker_image_deployment_yaml_file.sh
@@ -1,2 +1,1 @@
-awk -i inplace  -v var1="capstone-project3:pgdevops-BUILD_NUMBER" -v var2=capstone-project3:pgdevops-$1 '{gsub(var1,var2); print;}' gs-spring-boot-docker
-/deploy_k8s.yaml
+awk -i inplace  -v var1="capstone-project3:pgdevops-BUILD_NUMBER" -v var2=capstone-project3:pgdevops-$1 '{gsub(var1,var2); print;}' gs-spring-boot-docker/deploy_k8s.yaml
